e6d3675ff1b80fc18bc92cf2e4b30896b43b87b5
Xavi002 <diogomartins200214@gmail.com>
2020-10-30 22:43:30 +0000

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+
+import TouchScreen from './src/screens/TouchScreen'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <TouchScreen />
       <StatusBar style="auto" />
-    </View>
-  );
+    </>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/TouchScreen/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import { Container } from './styles'
+
+const TouchScreen = () => {
+  return( 
+    <Container>
+      <Text>Screen</Text>
+    </Container>
+  )
+}
+
+export default TouchScreen;
